ac5bcb955f2edc39bc2223f2424668eb986a491e
Hrafnkell Brimar Hallmundsson <hrafnkell@gmail.com>
2012-09-13 11:05:19 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musteri::Application.config.secret_token = 'b116693598f084d66c6d359251be7b927b3e9e1a8bf33cbc207bcb9a83631dde19c7637ae62faff77bea19442dc407bfca9b269e84a891c986459aec846a8dd1'
