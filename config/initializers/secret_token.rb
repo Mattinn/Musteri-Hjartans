2e49a90601f71a647209642e17b13d3f6f49383e
root <root@hrafnkell-server.lan>
2012-10-06 15:20:35 +0000

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musteri::Application.config.secret_token = '2f71107793b385975baa4f4135888b2df864f48be6cd1dea5fbe8ce7a731547413e27a05b84ace7af3f8f10b6514cdd8afa7df27d6e598652c5670a00b4ed7e6'
