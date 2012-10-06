ef62b5edcf36c8fe482869ec78decc01b98e5308
Matthias Leo Gislason <Matthiasg10@ru.is>
2012-10-06 17:21:28 +0000

    Made sessions model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Musteri::Application.routes.draw do
+  
   resources :users
+  resources :sessions
+  
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  
+  # root :to => "users#new"
+  # get "sessions/new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
