a4e455270a0fb060bffd2631a51f4b86ba1da676
hrafnkell <hrafnkellbrimar@gmail.com>
2012-10-10 11:29:44 +0000

    added account model, migration and admin attribute to user

--- db/migrate/20121010030216_add_active_to_users.rb
+++ db/migrate/20121010030216_add_active_to_users.rb
@@ -1,5 +1,6 @@
 class AddActiveToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :active, :boolean
+    add_column :users, :active, :boolean, default: false
+    add_column :users, :is_admin, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121010030216) do
+ActiveRecord::Schema.define(:version => 20121010111304) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "user_id"
+    t.string   "healer_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "healers", :force => true do |t|
     t.string   "name"
