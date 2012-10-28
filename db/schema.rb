4ef0dff60830f9346878dee1353a3da3b2dad027
hrafnkell <hrafnkellbrimar@gmail.com>
2012-10-28 16:59:19 +0000

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20121028155552) do
 
-  create_table "accounts", :force => true do |t|
-    t.string   "user_id"
-    t.string   "healer_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "books", :force => true do |t|
     t.string   "name"
     t.string   "author"
