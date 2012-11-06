154a2270fc17559e960b4ee158294c9d694fe918
hrafnkell <hrafnkellbrimar@gmail.com>
2012-11-06 14:29:57 +0000

    database changed for the better

--- db/migrate/20121010030216_add_active_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddActiveToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :active, :boolean, default: false
-    add_column :users, :is_admin, :boolean, default: false
-  end
-end
