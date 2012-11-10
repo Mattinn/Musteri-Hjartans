4dcb425176394b407410d25418c62d234251cc39
hrafnkell <hrafnkellbrimar@gmail.com>
2012-11-10 16:46:00 +0000

    added default => 0 to :answer in questions table

--- db/migrate/20121108173430_create_questions.rb
+++ db/migrate/20121108173430_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string :question_text
       t.float :value
-      t.integer :answer
+      t.integer :answer, :default => 0 
       t.belongs_to :personal_test
 
       t.timestamps
