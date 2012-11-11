eb49317757c68cab5b696e170ea80f683d8928a9
Matthias Leo Gislason <Matthiasg10@ru.is>
2012-11-11 11:57:33 +0000

    Added dynamic questions adding & removal

--- app/assets/javascripts/personal_tests.js.coffee
+++ app/assets/javascripts/personal_tests.js.coffee
@@ -2,17 +2,4 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-#Remouves a field from the element
-
-remove_fields = (link) ->
-  $(link).prev("input[type=hidden]").val "1"
-  $(link).closest(".fields").hide()
-  return
-
-#Adds a field with a unique id to the element
-add_fields = (link, association, content) ->
-  new_id = new Date().getTime()
-  regexp = new RegExp("new_" + association, "g")
-  $(link).parent().before content.replace(regexp, new_id)
-  return
   
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,4 @@ module ApplicationHelper
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
-  
-  #Helper that adds fields to the object that calls it (used to add question fields to the personal_test view)
-  def link_to_add_fields(name, f, association)
-  new_object = f.object.class.reflect_on_association(association).klass.new
-  fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-    render(association.to_s.singularize + "_fields", :f => builder)
-    end
-  link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
-  end
 end
