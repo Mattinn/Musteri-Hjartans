c7bc2ec619f57349ca7fe3bb6e25697b0437062f
Matthias Leo <matthiasg10@ru.is>
2012-11-25 22:38:23 +0000

    users can edit treatments

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -81,7 +81,6 @@ class MembersController < ApplicationController
     else
       redirect_to '/users/sign_in'
     end
-    
   end
 
   # POST /users
@@ -110,7 +109,7 @@ class MembersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html {  render action: "edit", notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
