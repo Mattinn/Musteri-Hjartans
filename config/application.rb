7449c49d0355f2c749a6c5acbb2104f68a2c7718
Matthias Leo <matthiasg10@ru.is>
2012-11-27 20:06:09 +0000

    Removed the app.path that was cosing crashes

--- config/application.rb
+++ config/application.rb
@@ -55,9 +55,6 @@ module Musteri
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    
-    # Add the devise paths
-    paths.app.views << "app/views/devise"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
