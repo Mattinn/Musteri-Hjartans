8d752ddf71ea2d016b91c5173115b5e5db309669
Matthias Leo <matthiasg10@ru.is>
2012-11-28 00:17:06 +0000

    Some translations

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(:version => 20121124173812) do
     t.integer  "question_id"
     t.integer  "answer"
     t.text     "text_answer"
-    t.float    "q_value"
-    t.string   "q_text"
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
   end
@@ -84,6 +82,7 @@ ActiveRecord::Schema.define(:version => 20121124173812) do
   create_table "questions", :force => true do |t|
     t.string   "question_text"
     t.float    "value"
+    t.integer  "answer"
     t.integer  "personal_test_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
@@ -92,9 +91,9 @@ ActiveRecord::Schema.define(:version => 20121124173812) do
   add_index "questions", ["personal_test_id"], :name => "index_questions_on_personal_test_id"
 
   create_table "results", :force => true do |t|
-    t.integer  "user_personal_test_session_id"
     t.float    "score"
     t.text     "message"
+    t.integer  "user_personal_test_session_id"
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
   end
@@ -125,6 +124,7 @@ ActiveRecord::Schema.define(:version => 20121124173812) do
   create_table "user_personal_test_sessions", :force => true do |t|
     t.integer  "user_id"
     t.integer  "personal_test_id"
+    t.integer  "question_id"
     t.integer  "category_id"
     t.integer  "treatment_id"
     t.integer  "result_id"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(:version => 20121124173812) do
 
   add_index "user_personal_test_sessions", ["category_id"], :name => "index_user_personal_test_sessions_on_category_id"
   add_index "user_personal_test_sessions", ["personal_test_id"], :name => "index_user_personal_test_sessions_on_personal_test_id"
+  add_index "user_personal_test_sessions", ["question_id"], :name => "index_user_personal_test_sessions_on_question_id"
   add_index "user_personal_test_sessions", ["result_id"], :name => "index_user_personal_test_sessions_on_result_id"
   add_index "user_personal_test_sessions", ["treatment_id"], :name => "index_user_personal_test_sessions_on_treatment_id"
   add_index "user_personal_test_sessions", ["user_id"], :name => "index_user_personal_test_sessions_on_user_id"
