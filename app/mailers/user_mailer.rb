b8655d1e111fdf9a20584d01dbe245367d483e8f
Matthias Leo <matthiasg10@ru.is>
2012-12-17 15:18:18 +0000

    renamed and made slight changes to the user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class UserMailer < Devise::Mailer
-  default from: "musterikaerleikans@gmail.com"
+  default from: "musterihjartans@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
